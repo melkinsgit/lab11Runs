927400d4397073a75989e7ed8d25170cf8816364
melkinsgit <margaret.elkins@gmail.com>
2016-04-14 20:59:09 -0500

    some index.js clean up

--- routes/index.js
+++ routes/index.js
@@ -53,7 +53,7 @@ router.post('/', function(req, res, next) {
 } );
 
 
-/* New POST */
+/* New POST to add a run to a lake */
 
 router.post('/addRun', function(req, res, next) {
 	
@@ -64,8 +64,6 @@ router.post('/addRun', function(req, res, next) {
 	
 	// lake will be a db object that is a match for the lake name the user chose
 	Lake.findOne ( { name: req.body.name }, function (err, lake) {
-		
-
 		
 		if (err) {
 				return next(err);
@@ -89,79 +87,5 @@ router.post('/addRun', function(req, res, next) {
 		});
 	} );
 });
-
-
-/* Delete a bird */ 
-// router.post('/deleteBird', function(req, res, next) {
-
-	// var birdToDelete = req.body.name;
-	
-	// Bird.findOne({name: req.body.name}, function (err, bird){
-		// if (err){
-			// return next(err)
-		// }
-		
-		// if (!bird) {
-			// return next (new Error('No bird found with name ' + req.body.name) );
-		// }
-		
-		// Bird.remove({name:req.body.name}, function (error, result) {
-			// if (err){
-				// return next(err)
-			// }
-			// res.redirect('/');
-		// });	
-	// });
-// } );
-
-
-/* Update a bird */
-// router.post('/updateBird', function(req, res, next) {
-	
-	// find the bird sent as hidden from the update button next to a given bird
-	// Bird.findOne({name: req.body.name}, function (err, birdToUpdate){
-		// if (err){
-			// return next(err)
-		// }
-		
-		// if (!birdToUpdate) {
-			// return next (new Error('No bird found with name ' + req.body.name) );
-		// }
-		// if there is no error and there is a bird by the name of the req data sent, then render the update form/page with the data for that bird - the object of which was returned from the findOne search
-		// console.log("the bird we want to update is:")
-		// console.log(birdToUpdate);		
-		// where the jade file refers to bird, the values from birdToUpdate will be used
-		// res.render('update', {bird: birdToUpdate});  // jade: code
-	// });
-// });
-
-/* Submit Updated bird */ 
-// router.post('/submitUpdate', function(req, res, next){
-	
-	// take the data from the form as updated by the user and set it into vars that will be used for the update method
-	// var updateInfo = {name:req.body.name[0],
-		// description: req.body.description,
-		// averageEggsLaid: req.body.averageEggsLaid,
-		// threatened: req.body.threatened}
-		
-	// the _id is name[1]; the bird name is name[0]
-	// search for the bird that has been updated which is the collected of data from the req - the id num has been sent as hidden from the form via the req
-	// Bird.findOne({_id:req.body.name[1]}, function (err, birdToSubmit){
-		// if (err){
-			// return next(err)
-		// }
-		// if (!birdToSubmit) {
-			// return next (new Error('No bird found with name ' + req.body.name) );
-		// }
-		// if no error and that bird by that id exists, update the object returned from the findOne with the data sent by the user via the request now in var updateInfo
-		// birdToSubmit.update(updateInfo, function (err){
-			// if (err) {
-				// return next(err)
-			// }
-			// reload the main page with updated data
-			// res.redirect('/');
-		// });
-	// });
-// });
 
 module.exports = router;
